ede5812977e8f2c84f2096d71c9695f63a3df834
qinxue.pan <xue@acrcloud.com>
2016-10-09 11:58:32 +0800

    init v1

---
+++ linux_arm/acr_extr_tool.h
@@ -0,0 +1,15 @@
+#ifndef _Included_acr_extr_tool_H
+#define _Included_acr_extr_tool_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int create_fingerprint(char *pcm_buffer, int pcm_buffer_len, char is_db_fingerprint, char **fps_buffer);
+void acr_free(char *buffer);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ linux_arm/x64/python2/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+'''
+    >>> python test.py test.mp3
+'''
+
+import os, sys
+from acrcloud.recognizer import ACRCloudRecognizer
+
+if __name__ == '__main__':
+    config = {
+        'host':'XXXXXX',
+        'access_key':'XXXXXX',
+        'access_secret':'XXXXXX',
+        'timeout':5
+    }
+    
+    '''This module can recognize ACRCloud by (RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, mono 8000 HZ) file. '''
+
+    re = ACRCloudRecognizer(config)
+    buf = open(sys.argv[1], 'rb').read()
+    print(re.recognize(buf))
