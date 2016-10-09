e530ba8ef372487a8d5027568260c39412bceb13
qinxue.pan <xue@acrcloud.com>
2016-10-09 14:22:26 +0800

    acr_extr_tool.h

--- linux_arm/acr_extr_tool.h
+++ linux_arm/acr_extr_tool.h
@@ -6,7 +6,7 @@ extern "C" {
 #endif
 
 int create_fingerprint(char *pcm_buffer, int pcm_buffer_len, char is_db_fingerprint, char **fps_buffer);
-void acr_free(char *buffer);
+void acr_free(char *fps_buffer);
 
 #ifdef __cplusplus
 }
